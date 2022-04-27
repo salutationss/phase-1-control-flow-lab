96064129fdbc28e1a000d9f631cd968423d05e44
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-04-27 10:41:00 -0400

    commit tree 7e4f6d1015531eb15c5e66c5e24f44cee115ea4d

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(trip){
+    if (trip <= '400') {
+      return 'This one is on me!'
+    } else if (trip <= '2000') {
+      return 'That will be twenty bucks.'
+    } else if ('2000' < trip && trip <= '2500') {
+      return 'I will gladly take your thirty bucks.'
+    } else {
+      return 'No can do.'
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+   return city === 'NYC' ? "Ok, sounds good." : "No go."; 
+}
+ 
+
+
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case 'generous': 
+        return "Thank you so much.";
+      case 'not as generous':
+        return "Thank you."; 
+      default:
+        return 'Bye.';
+
+    } 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
